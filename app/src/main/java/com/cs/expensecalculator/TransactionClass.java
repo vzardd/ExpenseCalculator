f76a3583c8f0d04c83ea27df80c212ca82ec42e9
Vishnu Thulasidoss <vishnuthulasidosss@gmail.com>
2022-04-09 16:31:25 +0530

    Initial commit

---
+++ app/src/main/java/com/cs/expensecalculator/TransactionClass.java
@@ -0,0 +1,37 @@
+package com.cs.expensecalculator;
+
+public class TransactionClass {
+    private int amount;
+    private String message;
+    private boolean positive;
+
+    public TransactionClass(int amount, String message, boolean positive) {
+        this.amount = amount;
+        this.message = message;
+        this.positive = positive;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isPositive() {
+        return positive;
+    }
+
+    public void setPositive(boolean positive) {
+        this.positive = positive;
+    }
+}
